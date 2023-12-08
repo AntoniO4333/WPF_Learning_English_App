2e573f524f1623e3825659a966ecefabfc4b5ec5
Anton Cheremushkin <anton43r3m@gmail.com>
2023-12-08 13:53:26 +0700

    Сделал окна макет окна изучения и добавления слов, связал окна друг с другом, сделал закрытие окон

---
+++ AddNewWordWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cheremushkinae_107d2
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewWordWindow.xaml
+    /// </summary>
+    public partial class AddNewWordWindow : Window
+    {
+        public AddNewWordWindow()
+        {
+            InitializeComponent();
+            Closing += AddNewWordWindow_Closing;
+        }
+
+        private void BackToMain_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Owner as MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.Show();
+                this.Hide();
+            }
+            else
+            {
+                mainWindow = new MainWindow();
+                mainWindow.Owner = this;
+                mainWindow.Show();
+                this.Hide();
+            }
+        }
+
+        private void StartLearning_Click(object sender, RoutedEventArgs e)
+        {
+            LearningWindow learningWindow = Owner as LearningWindow;
+            if (learningWindow != null)
+            {
+                learningWindow.Show();
+                this.Hide();
+            }
+            else
+            {
+                learningWindow = new LearningWindow();
+                learningWindow.Owner = this;
+                learningWindow.Show();
+                this.Hide();
+            }
+        }
+
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNewWordWindow_Closing(object sender, CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
--- LearningWindow.xaml.cs
+++ LearningWindow.xaml.cs
@@ -11,57 +11,39 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Cheremushkinae_107d2
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для LearningWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LearningWindow : Window
     {
-        public MainWindow()
+        public LearningWindow()
         {
             InitializeComponent();
-            Closing += MainWindow_Closing;
-            this.ResizeMode = ResizeMode.NoResize;
-
+            Closing += LearningWindow_Closing;
         }
 
-        private void SettingsMain_Click(object sender, RoutedEventArgs e)
+        private void BackToMain_Click(object sender, RoutedEventArgs e)
         {
-            
-        }
-
-        private void SignInMain_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void SignUpMain_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void StartLearningMain_Click(object sender, RoutedEventArgs e)
-        {
-            LearningWindow learningWindow = Owner as LearningWindow;
-            if (learningWindow != null)
+            MainWindow mainWindow = Owner as MainWindow;
+            if (mainWindow != null)
             {
-                learningWindow.Show();
+                mainWindow.Show();
                 this.Hide();
             }
             else
             {
-                learningWindow = new LearningWindow();
-                learningWindow.Owner = this;
-                learningWindow.Show();
+                mainWindow = new MainWindow();
+                mainWindow.Owner = this;
+                mainWindow.Show();
                 this.Hide();
             }
         }
 
-        private void AddNewWordMain_Click(object sender, RoutedEventArgs e)
+        private void AddNewWordLearning_Click(object sender, RoutedEventArgs e)
         {
             AddNewWordWindow addNewWordWindow = Owner as AddNewWordWindow;
             if (addNewWordWindow != null)
@@ -79,9 +61,15 @@ namespace Cheremushkinae_107d2
         }
 
 
-        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        private void SettingsLearning_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LearningWindow_Closing(object sender, CancelEventArgs e)
         {
             Application.Current.Shutdown();
         }
+
     }
 }
